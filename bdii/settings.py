fbfaf7f9883f09cb60ccc86abec36fddd97ba979
giannimastroscianni <giannisoad12@gmail.com>
2017-06-17 17:38:59 +0200

    db deleted

--- bdii/settings.py
+++ bdii/settings.py
@@ -80,10 +80,7 @@ WSGI_APPLICATION = 'bdii.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
 }
 
 
